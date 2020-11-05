182fc1588340424fab8e4fe4454729e400dd6892
weverton-vitor <wevertonvitor01@gmail.com>
2020-11-05 20:56:08 -0300

    Add TaskList and TaskListItem  models

--- project/apps/list_of_items/models.py
+++ project/apps/list_of_items/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from project.apps.core.models import TimeStampedModel
 
-# Create your models here.
+
+class TaskList(TimeStampedModel):
+    title = models.CharField(max_length=100)
+    
+    def __str__(self):
+         return self.title
+
+
+class TaskListItem(models.Model):
+    task_list = models.ForeignKey(to=TaskList, on_delete=models.CASCADE)
+    description = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.task_list.title} - {self.descrition}"
