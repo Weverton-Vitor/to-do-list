3fbb005275e12a5e207b449690185b3fb3285b37
weverton-vitor <wevertonvitor01@gmail.com>
2020-11-05 20:54:02 -0300

    Add Annotation model

--- project/apps/annotations/models.py
+++ project/apps/annotations/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from project.apps.core.models import TimeStampedModel
 
-# Create your models here.
+
+class Annotation(TimeStampedModel):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # 1 for red, 2 for orange and 3 for green
+    priority = models.PositiveSmallIntegerField()
+    
+    
+    def __str__(self):
+        return f"{self.title} - {self.descrition}"
