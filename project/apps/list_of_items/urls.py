e939c015fe888c4aef9279cf0b0be59f3a4f5ca8
weverton-vitor <wevertonvitor01@gmail.com>
2020-12-12 08:23:00 -0300

    Adding TaskListDeleteView to Task List

--- project/apps/list_of_items/urls.py
+++ project/apps/list_of_items/urls.py
@@ -4,5 +4,7 @@ from project.apps.list_of_items import views
 app_name='list_of_items'
 
 urlpatterns = [
-    path('', views.TaskListListView.as_view(), name='task_list_list')
+    path('', views.TaskListListView.as_view(), name='task_list_list'),
+    path('Deletar/<int:pk>', views.TaskListDeleteView.as_view(), name='task_list_delete'),
+
 ]
--- project/apps/list_of_items/views.py
+++ project/apps/list_of_items/views.py
@@ -1,6 +1,8 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import DeleteView, ListView
 from project.apps.list_of_items.models import TaskList
 
 
@@ -51,3 +53,28 @@ class TaskListListView(ListView):
             queryset = queryset.filter(title__istartswith=title)
 
         return queryset
+
+
+class TaskListDeleteView(DeleteView):
+    model = TaskList
+    success_url = reverse_lazy('list_of_items:task_list_list')
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponseRedirect(reverse('list_of_items:task_list_list') + '?' + self.request.GET.urlencode)       
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+        success = self.object.delete()
+        if success[0]:
+            messages.success(
+                self.request, f'Sucesso ao deletar {self.object.title}')
+
+        return HttpResponseRedirect(success_url)
+    
+    def get_success_url(self):
+        # Verificando qual será a ordem da listagem pela QueryString da requisição        
+        if self.request.GET.get('change'):
+            return reverse('list_of_items:task_list_list') + '?change=order'
+
+        return reverse('list_of_items:task_list_list')
